6e6007f7d1fb7426f82195ec899eb23ac3c4f2c8
yonggyo1125 <yonggyo00@kakao.com>
2024-02-21 12:17:22 +0900

    회원가입 구현중

--- src/main/java/org/choongang/member/service/MemberSaveService.java
+++ src/main/java/org/choongang/member/service/MemberSaveService.java
@@ -1,9 +1,11 @@
 package org.choongang.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.constants.Authority;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +13,22 @@ import org.springframework.stereotype.Service;
 public class MemberSaveService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder encoder;
 
     public void join(RequestJoin form) {
 
+        String hash = encoder.encode(form.getPassword());
+
+        Member member = Member.builder()
+                .email(form.getEmail())
+                .name(form.getName())
+                .password(hash)
+                .authority(Authority.USER)
+                .lock(false)
+                .enable(true)
+                .build();
+
+        save(member);
     }
 
     public void save(Member member) {

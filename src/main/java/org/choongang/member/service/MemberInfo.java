05c353148a2c305d2a0a454142a417afc5a68f25
yonggyo1125 <yonggyo00@kakao.com>
2024-02-21 11:28:23 +0900

    회원가입 구현중

---
+++ src/main/java/org/choongang/member/service/MemberInfo.java
@@ -0,0 +1,56 @@
+package org.choongang.member.service;
+
+import lombok.Builder;
+import lombok.Data;
+import org.choongang.member.entities.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+public class MemberInfo implements UserDetails {
+
+    private String email;
+    private String password;
+    private Member member;
+    private boolean enable;
+    private boolean lock;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !lock;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
+}

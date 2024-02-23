f8db7ec084350bdb382120b56fd68d131d925f4b
yonggyo1125 <yonggyo00@kakao.com>
2024-02-23 10:01:12 +0900

    회원가입 구현중

--- src/main/java/org/choongang/member/controllers/JoinValidator.java
+++ src/main/java/org/choongang/member/controllers/JoinValidator.java
@@ -3,6 +3,7 @@ package org.choongang.member.controllers;
 import lombok.RequiredArgsConstructor;
 import org.choongang.member.repositories.MemberRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -25,7 +26,22 @@ public class JoinValidator implements Validator {
          * 3. 비밀번호, 비밀번호 확인 일치여부
          */
 
-        // 1. 이메일 중복 여부
+        RequestJoin form = (RequestJoin)target;
+        String email = form.getEmail();
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
 
+        // 1. 이메일 중복 여부
+        if (StringUtils.hasText(email) && repository.exists(email)) {
+            errors.rejectValue("email", "Duplicated");
+        }
+
+        // 2. 비밀번호 복잡성 - 대소문자 각각 1자 이상 + 숫자 + 특수문자
+
+        // 3. 비밀번호, 비밀번호 확인 일치여부
+        if (StringUtils.hasText(password) && StringUtils.hasText(confirmPassword)
+            && !password.equals(confirmPassword)) {
+            errors.rejectValue("confirmPassword", "Mismatch");
+        }
     }
 }

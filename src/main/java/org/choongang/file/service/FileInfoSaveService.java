16d8fd139e0846127ec17ac7df89eaa31608a385
yonggyo1125 <yonggyo00@kakao.com>
2024-02-28 09:13:11 +0900

    파일 업로드 구현

--- src/main/java/org/choongang/file/service/FileInfoSaveService.java
+++ src/main/java/org/choongang/file/service/FileInfoSaveService.java
@@ -3,6 +3,7 @@ package org.choongang.file.service;
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.controllers.RequestFileUpload;
 import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,9 @@ import java.util.Objects;
 @Service
 @RequiredArgsConstructor
 public class FileInfoSaveService {
+
+    private final FileInfoRepository fileInfoRepository;
+
     public List<FileInfo> save(RequestFileUpload form) {
         List<FileInfo> items = new ArrayList<>();
         MultipartFile[] files = form.getFile();
@@ -21,23 +25,31 @@ public class FileInfoSaveService {
         boolean imageOnly = Objects.requireNonNullElse(form.getImageOnly(), false);
 
         for (MultipartFile file : files) {
-            // 파일명.확장자, 파일명.파일명.확장자
+            // 파일명.확장자, 파일명.파일명.확장자 - lastIndexOf
             String fileName = file.getOriginalFilename();
+            String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+            String contentType = file.getContentType(); // image/jpeg, image/png ...
+
+            // 이미지만 업로드일때 이미지가 아닌 파일은 건너뛰기
+            if (imageOnly && contentType.indexOf("image/") == -1) {
+                continue;
+            }
 
             FileInfo item = FileInfo.builder()
                     .gid(gid)
                     .location(location)
                     .fileName(fileName)
-                    .contentType(file.getContentType())
+                    .contentType(contentType)
+                    .extension(extension)
+                    .file(file)
                     .build();
-                    //.extension(extension)
 
-
+            items.add(item);
         }
 
 
+        fileInfoRepository.saveAllAndFlush(items);
 
-
-        return null;
+        return items;
     }
 }

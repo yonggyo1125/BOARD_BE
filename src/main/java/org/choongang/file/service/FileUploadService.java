bb9e55fb51c956025b62ac2db68d405fe8491760
yonggyo1125 <yonggyo00@kakao.com>
2024-02-28 09:51:05 +0900

    파일 업로드 구현

--- src/main/java/org/choongang/file/service/FileUploadService.java
+++ src/main/java/org/choongang/file/service/FileUploadService.java
@@ -1,17 +1,26 @@
 package org.choongang.file.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.configs.FileProperties;
 import org.choongang.file.controllers.RequestFileUpload;
 import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
 public class FileUploadService {
     private final FileInfoSaveService saveService;
+    private final FileProperties props;
+    private final FileInfoRepository repository;
 
     public List<FileInfo> upload(RequestFileUpload form) {
 
@@ -19,6 +28,29 @@ public class FileUploadService {
 
         for (FileInfo item : items) {
             MultipartFile file = item.getFile();
+
+            long seq = item.getSeq();
+            String uploadDir = props.getPath() + (seq % 10L);
+            File dir = new File(uploadDir);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            String extension = item.getExtension();
+            File uploadFile = new File(uploadDir + "/" + seq +
+                    (StringUtils.hasText(extension) ? "." + extension : ""));
+            try {
+                file.transferTo(uploadFile);
+                // 파일 부가정보 - 파일 path, 파일 url
+
+            } catch (IOException e) {
+                // 업로드 실패 -> 파일 정보 제거
+                repository.delete(item);
+                items.remove(item);
+            }
         }
+
+        repository.flush();
+
+        return items;
     }
 }
